e7d8f6132589505ad05aa62a51ccbfa7ee402bee
Mohamed Samir <tchd1@charisma-design.eu>
2021-12-02 10:51:42 +0200

    output input and string conctenation

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your first Name");
+        string FirstName = Console.ReadLine();
+        Console.WriteLine("Enter your last Name");
+        string LastName = Console.ReadLine();
+        Console.WriteLine("Your Name is {0}  {1}",FirstName,LastName);
+    }
+
+    static void test(int test)
+    {
+        Console.WriteLine(test);
+    }
+}
+
+
+
